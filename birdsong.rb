26932c81367cbc28a35a133db77cc9aed7367c8d
unknown <dparkinson@DLTJDT8H12.ad.laterooms.com>
2014-10-25 17:44:28 +0100

    added birds011.wav to sounds, plays sound in playingwithsound.rb

--- birdsong.rb
+++ birdsong.rb
@@ -1,1 +1,16 @@
 #birdsong.rb
+require 'net/https'
+require 'clockwork'
+ 
+api = Clockwork::API.new('6f7e73dd28bf6022c1a988a884c880f283830ece')
+message = api.messages.build( :to => '447731090123', :content => 'Test text' )
+response = message.deliver
+ 
+if response.success
+    puts response.message_id
+else
+    puts response.error_code
+    puts response.error_description
+end
+
+
---
+++ playingwithsound.rb
@@ -0,0 +1,24 @@
+#!/bin/env ruby
+# encoding: utf-8
+require 'win32/sound'
+include Win32
+asciiChars = []
+inputString = "thisisastringthatiwanttomakelonger to test mor!!£$%215235352412P{}@P_+-=-';/;["
+
+h = { 
+	0 => "0.wav", 
+	1 => "1.wav",
+	2 => "2.wav" }
+
+puts h.fetch(1)
+inputChars = inputString.split(//)
+inputChars.each do |char|
+	asciiChars.push(char.ord)
+end
+
+asciiChars.each do |ascii|
+	puts ascii%12
+end
+
+#Sound.play("sounds/birds011")
+
