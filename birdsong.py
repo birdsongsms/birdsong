feb32bd32176e3c0bf555147d7687eb431d2fd50
Peter Jacobson <peterjacobson37@gmail.com>
2014-10-25 17:02:39 +0100

    "new web text"

---
+++ birdsong.py
@@ -0,0 +1,13 @@
+import webapp2
+import os
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+    	self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, Ben Parish! We are building a prototype web app first!)
+
+		
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
